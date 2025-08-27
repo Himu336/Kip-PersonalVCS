8af9afc3a3018d9cdd6baca7d3fa1eef1f0c22cf
Himansh <himanshbansal200521@gmail.com>
2025-08-27 14:32:58 +0530

    setup personal version control system

---
+++ Kip.mjs
@@ -0,0 +1,180 @@
+#!/usr/bin/env node
+
+import path from 'path';
+import fs from 'fs/promises';
+import crypto from 'crypto';
+import { diffLines } from 'diff';
+import chalk from 'chalk';
+import { Command } from 'commander';
+
+const program = new Command();
+
+class Kip {
+
+    constructor(repoPath ='.') {
+        this.repoPath = path.join(repoPath, '.kip');
+        this.objectsPath = path.join(this.repoPath, 'objects'); //.kip/objects
+        this.headPath = path.join(this.repoPath, 'HEAD'); //.kip/HEAD
+        this.indexPath = path.join(this.repoPath, 'index'); //.kip/index
+    }
+
+    async init() {
+        await fs.mkdir(this.objectsPath, { recursive: true });
+        try {
+            await fs.writeFile(this.headPath,'', {flag: 'wx'});
+            await fs.writeFile(this.indexPath, JSON.stringify([]), {flag: 'wx'});
+        } catch (error) {
+            console.log("Already initialized the .kip folder");
+        }
+    }
+
+    hashObject(content) {
+        return crypto.createHash('sha1').update(content, 'utf-8').digest('hex');
+    }
+
+    async add(fileToBeAdded) {
+        const fileData = await fs.readFile(fileToBeAdded, { encoding: 'utf-8'}); //read the file
+        const fileHash = this.hashObject(fileData); //hash the file
+
+        const newFileHashedObjectPath = path.join(this.objectsPath, fileHash); // .kip/objects/abc123
+        await fs.writeFile(newFileHashedObjectPath, fileData);
+        await this.updateStagingArea(fileToBeAdded, fileHash);
+    }
+
+    async updateStagingArea(filePath, fileHash) {
+        const index = JSON.parse(await fs.readFile(this.indexPath, { encoding: 'utf-8'})); // read the index file
+        index.push({ path: filePath, hash: fileHash}); //add the file to the index
+        await fs.writeFile(this.indexPath, JSON.stringify(index)); //write the updated index file
+    }
+
+    async commit(message) {
+        const index = JSON.parse(await fs.readFile(this.indexPath, { encoding: 'utf-8'}));
+        const parentCommit = await this.getCurrentHead();
+        
+        const commitData = {
+            timeStamp: new Date().toISOString(),
+            message,
+            files: index,
+            parent: parentCommit
+        };
+
+        const commitHash = this.hashObject(JSON.stringify(commitData));
+        const commitPath = path.join(this.objectsPath, commitHash);
+        await fs.writeFile(commitPath, JSON.stringify(commitData));
+        await fs.writeFile(this.headPath, commitHash); // update head to point to new commit 
+        await fs.writeFile(this.indexPath, JSON.stringify([])); // clea the staging area
+        console.log(`Commit successfully created: ${commitHash}`);
+    }
+
+    async getCurrentHead() {
+        try {
+            return await fs.readFile(this.headPath, { encoding: 'utf-8'});
+        } catch (error) {
+            return null;
+        }
+    }
+
+    async log() {
+        let currentCommitHash = await this.getCurrentHead();
+        while(currentCommitHash) {
+            const commitData = JSON.parse(await fs.readFile(path.join(this.objectsPath, currentCommitHash ), {
+                encoding: 'utf-8'
+            }));
+            console.log(`---------------------\n`);
+        console.log(`Commit: ${currentCommitHash}\nDate: ${commitData.timeStamp}\n\nMessage: ${commitData.message}\n\n`);
+        currentCommitHash = commitData.parent;
+        };
+    }
+
+    async showCommitDiff(commitHash) {
+        const commitData = JSON.parse(await this.getCommitData(commitHash));
+        if(!commitData) {
+            console.log("Commit not found");
+            return;
+        }
+        console.log("Changes in the last commit are:");
+
+        for(const file of commitData.files) {
+            console.log(`File: ${file.path}`);
+            const fileContent = await this.getFileContent(file.hash);
+            console.log(`Content: ${fileContent}`);
+
+            if(commitData.parent) {
+                //get parent commit data
+                const parentCommitData = JSON.parse(await this.getCommitData(commitData.parent));
+                const parentFileContent = await this.getParentFileContent(parentCommitData, file.path);
+                if(parentFileContent != undefined) {
+                    console.log('\nDiff:');
+                    const diff = diffLines(parentFileContent, fileContent);
+
+                    console.log(diff);
+
+                    diff.forEach(part => {
+                        if(part.added) {
+                            process.stdout.write(chalk.green("++" + part.value));
+                        } else if(part.removed) {
+                            process.stdout.write(chalk.red("--" + part.value));
+                        } else {
+                            process.stdout.write(chalk.grey(part.value));
+                        }
+                    });
+                    console.log(); // new line
+                } else {
+                    console.log("New file in this commit");
+                }
+            } else {
+                console.log("First Commit");
+            }
+        }
+    }
+    
+
+    async getParentFileContent(parentCommitData, filePath) {
+        const parentFile = parentCommitData.files.find(file => file.path === filePath);
+        if(parentFile) {
+            return  await this.getFileContent(parentFile.hash);
+        }
+    }
+
+    async getCommitData(commitHash) {
+        const commitPath = path.join(this.objectsPath, commitHash);
+        try {
+            return await fs.readFile(commitPath, { encoding: 'utf-8'});
+        } catch (error) {
+            console.log("Failed to read the commit data", error);
+            return null;
+        }
+    }
+
+    async getFileContent(fileHash) {
+        const objectPath = path.join(this.objectsPath, fileHash);
+        return fs.readFile(objectPath, { encoding: 'utf-8'});
+    }
+}
+
+program.command('init').action(async () => {
+    const kip = new Kip();
+    await kip.init();
+});
+
+program.command('add <file>').action(async (file) => {
+    const kip = new Kip();
+    await kip.add(file);
+});
+
+program.command('commit <message>').action(async (message) => {
+    const kip = new Kip();
+    await kip.commit(message);
+});
+
+program.command('log').action(async () => {
+    const kip = new Kip();
+    await kip.log();
+});
+
+program.command('show <commitHash>').action(async (commitHash) => {
+    const kip = new Kip();
+    await kip.showCommitDiff(commitHash);
+});
+
+program.parse(process.argv);
